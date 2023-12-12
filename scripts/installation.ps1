0a0d664ed96365197dfa2fa2a932a0c667a76885
NilsVreman <nils.vreman@gmail.com>
2023-12-12 11:35:21 +0100

    Minor refactorizations

--- scripts/installation.ps1
+++ scripts/installation.ps1
@@ -1,24 +1,19 @@
 Write-Host "Starting the installation process..."
 
 # Check if Python is installed
-if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
-    Write-Host "Python is not installed. Installing Python..."
-    # Download Python Installer
-    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe" -OutFile "pythonInstaller.exe"
-    # Run Installer
-    Start-Process -FilePath .\pythonInstaller.exe -Args "/quiet InstallAllUsers=1 PrependPath=1" -Wait
-    Remove-Item .\pythonInstaller.exe
-}
+Write-Host "Installing Python..."
+# Download Python Installer
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe" -OutFile "python-3.12.1-amd64.exe"
+# Run Installer
+Start-Process -FilePath .\python-3.12.1-amd64.exe -Args "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+Remove-Item .\python-3.12.1-amd64.exe
 
-# Check if Node.js is installed
-if (-not (Get-Command node -ErrorAction SilentlyContinue)) {
-    Write-Host "Node.js is not installed. Installing Node.js..."
-    # Download Node.js Installer
-    Invoke-WebRequest -Uri "https://nodejs.org/dist/v20.10.0/node-v20.10.0-x64.msi" -OutFile "nodeInstaller.msi"
-    # Run Installer
-    Start-Process -FilePath .\nodeInstaller.msi -Args "/quiet" -Wait
-    Remove-Item .\nodeInstaller.msi
-}
+Write-Host "Installing Node.js..."
+# Download Node.js Installer
+Invoke-WebRequest -Uri "https://nodejs.org/dist/v20.10.0/node-v20.10.0-x64.msi" -OutFile "node-v20.10.0-x64.msi"
+# Run Installer
+Start-Process -FilePath .\node-v20.10.0-x64.msi -Args "/quiet" -Wait
+Remove-Item .\node-v20.10.0-x64.msi
 
 # Auxiliary variables
 $currentPath = Get-Location
