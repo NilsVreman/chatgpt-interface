2d8eb9a6c38f50816fb283ba577db47bd1b8b4fa
Nils Vreman <nils.vreman@se.com>
2023-12-13 11:25:44 +0100

    Added entire Node_modules folder to avoid having to compile in windows -> Allowing people without admin privileges to run app

--- scripts/start_application.ps1
+++ scripts/start_application.ps1
@@ -1,34 +1,3 @@
-function Set-SeProxy {
-
-    $proxyUrl = 'http://gateway.schneider.zscaler.net:9480'
- 
-    # # no_proxy needed for e.g. Selenium to start the ChromeDriver when proxy is set
-    $noProxy = 'localhost,127.0.0.0/8,::1,schneider-electric.com,10.0.0.0/8' 
-    setx NO_PROXY $noProxy
-
-    # http_proxy needed for Cypress to run correctly, for some reason it's not picking up https_proxy
-    setx HTTP_PROXY $proxyUrl
-
-    # https_proxy set just for the sake of it, see http_proxy for Cypress specifics
-    setx HTTPS_PROXY $proxyUrl
-
-    # Not sure why yarn does not use https_proxy from the environment, it should
-    setx YARN_HTTPS_PROXY $proxyUrl
-
-    #$zscalerCertPath = "$env:userprofile\zscaler.cer"
-    #if (!(test-path -path $zscalerCertPath)) {
-    #    Write-Host -ForegroundColor Red "Warning, the '$zscalerCertPath' file does not exist." `
-    #        "Search for the file name on the wiki to find out how to get a hold of it." 
-    #  }
-    ## I've tried to get this to work with both yarn's config for caFilePath and httpsCertFilePath but no luck
-    ## Leaving like this for now, it works :)
-    ## Looks to be an issue on yarn: https://github.com/yarnpkg/berry/issues/2250
-    #setx NODE_EXTRA_CA_CERTS $zscalerCertPath
-
-    write-host "Done!"
-    write-host "Remember that some applications (eg the terminal) need a restart to use the updated environment variables."
-}
-
 # Auxiliary variables
 $currentPath = $PSScriptroot
 $clientPath = Join-Path $PSScriptroot "..\client"
@@ -40,16 +9,9 @@ $serverCommand = "python chat_gpt_communication_server.py --api-base https://ope
 # Command to start the Node.js client
 $clientCommand = "npm start"
 
-Set-SeProxy
-
 Write-Host "Installing Python dependencies..."
 cd $serverPath
 pip install -r requirements.txt
-Write-Host " "
-
-Write-Host "Installing Node.js dependencies..."
-cd $clientPath
-npm install
 Write-Host " "
 
 cd $currentPath
